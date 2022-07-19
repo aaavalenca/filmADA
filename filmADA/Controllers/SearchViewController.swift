9b7bcf008d36031a9951044581cb582d34315287
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2022-07-19 20:25:16 -0300

    Segue details search

--- filmADA/Controllers/SearchViewController.swift
+++ filmADA/Controllers/SearchViewController.swift
@@ -51,5 +51,21 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         return cell ?? SearchTableViewCell()
         
     }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DetailsViewController {
+            let movie = sender as? Movie
+            destination.movie = movie
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let movie: Movie = searchedMovies[indexPath.item]
+        print(movie.title)
+        self.performSegue(withIdentifier: "detailSegue", sender: movie)
+        
+    }
+    
 
 }
